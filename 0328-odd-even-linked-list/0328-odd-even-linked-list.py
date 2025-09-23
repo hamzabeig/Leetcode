9df5bf20fdac6d62283e7d9d4bbcc1c7e90e6c8c
Hamza Baig <161630144+hamzabeig@users.noreply.github.com>
2025-09-23 21:31:44 +0500

    Time: 0 ms (100%), Space: 19.1 MB (73.97%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -4,48 +4,18 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    """
-    tmp = odd.next
-    odd.next = even.next
-    even.next = even.next.next
-    odd.next.next = tmp
-    odd = odd.next
-    even = even.next
-
-    -------------------------------------------------
-    1 -> 2 -> 3 -> 4 -> 5 -> 6 -> None
-    odd = 1
-    even = 2
-
-    1 -> 3 -> 2 -> 4 -> 5 -> 6 -> None
-    odd = 3
-    even = 4
-
-    1 -> 3 -> 5 -> 2 -> 4 -> 6 -> None
-    odd = 5
-    even = 6
-
-    1 -> 3 -> 5 -> 2 -> 4 -> 6 -> None
-
-    -------------------------------------------------
-    1 -> 2 -> 3 -> None
-    odd = 1
-    even = 2
-
-    1 -> 3 -> 2 -> None
-    odd = 3
-    even = None
-    """
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
-        odd = head
-        even = head.next
+        if not head or not head.next:
+            return head
+        
+        odd, even = head, head.next
+        even_head = even
+        
         while even and even.next:
-            tmp = odd.next
             odd.next = even.next
-            even.next = even.next.next
-            odd.next.next = tmp
             odd = odd.next
+            even.next = even.next.next
             even = even.next
+
+        odd.next = even_head  # Connect odd list to even list
         return head
