80417056c04b2ba5529a4ec5c5a7ea312ca97e0a
Hamza Baig <161630144+hamzabeig@users.noreply.github.com>
2026-04-22 17:35:56 +0500

    Time: 0 ms (100%), Space: 20.7 MB (10.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def itr_reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         tmp = head
         prev = None
         curr = head
@@ -14,6 +14,23 @@ class Solution:
             prev = curr
             curr = tmp
         return prev
-            
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # print(head)
+        if head == None:
+            return head
+
+        if head.next== None:
+            return head
+
+        new_head = self.reverseList(head.next)
+        # print(head)
+        head.next.next = head
+        head.next = None
+        
+        
+        return new_head
+        
+        
+        
 
         
