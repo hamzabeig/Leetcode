6c28b295e7a3500eb848f23aab89ec7a9dfdbc06
Hamza Baig <161630144+hamzabeig@users.noreply.github.com>
2026-04-22 16:41:58 +0500

    Time: 0 ms (100%), Space: 19.4 MB (45.25%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,24 +1,64 @@
 class Solution:
+    def itr_decodeString(self, s: str) -> str:
+        stack = []
+        curr_num = 0
+        curr_str =""
+
+        for ch in s:
+            if ch.isnumeric():    # ch.isdigit()  -- only for 0-9, better here
+                curr_num = curr_num * 10 + int(ch)
+
+            elif ch == "[":
+                # inner  = # level of recursion                 
+                stack.append((curr_str, curr_num))
+                curr_num = 0
+                curr_str =""
+
+            elif ch == "]":
+                # return recursion acc
+                pre_str, num = stack.pop()
+                curr_str = pre_str + curr_str * num
+                
+            else:
+                curr_str += ch
+
+        
+        return curr_str
+
+
     def decodeString(self, s: str) -> str:
         self.i = 0
+
         def decode():
-            result = ""
             num = 0
+            result = ""
+
             while self.i < len(s):
-                if s[self.i].isdigit():
-                    num = num * 10 + int(s[self.i])
+
+                ch = s[self.i]
+
+                if ch.isdigit():
                     self.i+=1
-                elif s[self.i] == "[":
+                    num = num * 10 + int(ch)
+                elif ch == "[":
                     self.i+=1
                     inner = decode()
-                    result += inner*num
-                    num = 0
-                elif s[self.i]=="]":
+                    
+                    result += num * inner
+                    num=0
+                    # result=""
+                    
+
+                elif ch == "]":
                     self.i+=1
                     return result
                 else:
-                    result = result + s[self.i]
-                    self.i += 1
-            return result  
-        return decode()                  
+                    self.i+=1
+                    result+=ch
+            return result
+        return decode()
 
+
+
+
+        
