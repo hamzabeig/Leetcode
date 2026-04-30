2ee22069670efa66b9dacdd10d719123e0c1ca57
Hamza Baig <161630144+hamzabeig@users.noreply.github.com>
2026-04-30 12:40:47 +0500

    Time: 3 ms (11.53%), Space: 19.4 MB (54.93%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -15,17 +15,43 @@ class Solution:
 
 #-----------------------------------------------------------
 # Recusrion
-        if n < 0:
-            x=1/x
-            n=-n
-            
-        if n == 0: 
-            return 1
+        # if n < 0:
+        #     x=1/x
+        #     n=-n
 
-        h = self.myPow(x,n//2)
+        # if n == 0: 
+        #     return 1
+
+        # h = self.myPow(x,n//2)
         
-        if n % 2 ==0:
-            return h * h
-        else:
-            return x * h * h
+        # if n % 2 ==0:
+        #     return h * h
+        # else:
+        #     return x * h * h
         
+#---------------------------------
+        '''
+        in binary we can represent any number in powers of two
+        10 = 1010 in binary
+        x^10 = x^8 + x^2
+        so we add keep on increasing power of x and add in result 
+        once least bit is 1 by cheking if n is odd.
+        we keeping on dividing powers n by 2 to shift right
+        and we check if least bit is set by checking if its odd
+        then we add that power of x to result, otherwise increase x but
+        don't add in result once least bit is 0
+        '''
+        def fast_pow(x, n):
+            res = 1.0
+            while n:
+                if n % 2 == 1:
+                    res *= x
+                x *= x
+                n //= 2
+            return res
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        return fast_pow(x, n)
