d08afb73f1381d785142473bd2fc986100ace541
Hamza Baig <161630144+hamzabeig@users.noreply.github.com>
2026-05-11 18:53:01 +0500

    Time: 15 ms (26.92%), Space: 23.4 MB (9.18%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,13 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+#         if not lists:
+#             return None
+#         n = len(lists)
+#         if n < 2:
+#             return lists[0]
+        
+        
+        
+#         def recursion(output, r):
+#             if r>=n:
+#                 return output
+#             output = merge(output, lists[r])
+
+#             return recursion(output, r+1)
+
+        # return recursion(lists[0],1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-        n = len(lists)
-        if n < 2:
-            return lists[0]
         
         def merge(cl,cr):
             if not cl:
@@ -23,15 +44,18 @@ class Solution:
                 cr.next = merge(cl,cr.next)
                 return cr
         
-        def recursion(output, r):
-            if r>=n:
-                return output
-            output = merge(output, lists[r])
-
-            return recursion(output, r+1)
-
-        return recursion(lists[0],1)
         
+        while len(lists) > 1:
+            merged_lists = []
+            
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged_lists.append(merge(l1, l2))
+            
+            lists = merged_lists
+        
+        return lists[0]
 
 
         
